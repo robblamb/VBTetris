4c1c8ad15ee80728dcbfd915fe9ab2f14c84dd59
robblamb <robblamb@hotmail.com>
2013-11-13 10:46:00 -0800

    added an options screen that does nothing and is never called

---
+++ src/vbtetris/VBTetrisOptionsScreen.java
@@ -0,0 +1,26 @@
+package vbtetris;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class VBTetrisOptionsScreen extends JPanel {
+	
+	private final int FRAME_WIDTH_PX;
+	private final int FRAME_HEIGHT_PX;
+	
+	public VBTetrisOptionsScreen(int w, int h, int p) {
+		setBackground(new Color(0, 0, 0, 0));
+		FRAME_WIDTH_PX = w;
+		FRAME_HEIGHT_PX = h;
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Color color = new Color(0, 0, 255, 150); 
+		g.setColor(color);
+		g.fillRect(0, 0, FRAME_WIDTH_PX, FRAME_HEIGHT_PX);
+	}
+	
+}
