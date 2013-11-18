ae20ef66fc33d2c2e880e2310586ca35e8fd028e
robblamb <robblamb@hotmail.com>
2013-11-17 21:40:05 -0800

    Added the slow downs to the timer.

--- src/vbtetris/VBTetrisTimer.java
+++ src/vbtetris/VBTetrisTimer.java
@@ -13,11 +13,23 @@ public class VBTetrisTimer extends Timer {
 		this.speedupby(10);
 	}
 	
-	// reduces speed by the given percent
+	// increases speed by the given percent
 	public void speedupby(int percent){
 		double newSpeed = this.getDelay();
 		newSpeed = newSpeed*(100-percent)/100;
 		this.setDelay((int)newSpeed);
+	}
+	//  slows down speed by 10%
+	public void slowDown(){
+		double newSpeed = this.getDelay();
+		newSpeed *=1.1;
+		this.setDelay((int) newSpeed);
+	}
+	// slows down speed by given pervent
+	public void slowDown(int percent){
+		double newSpeed = this.getDelay();
+		newSpeed *= 1 + (double)(percent/100);
+		this.setDelay((int) newSpeed);
 	}
 
 }
