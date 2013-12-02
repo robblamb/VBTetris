38fa894feb8d633bbacfd3cd80c3964f6c60ae7c
ereale <elijahreale@gmail.com>
2013-12-02 03:32:10 -0800

    Task tags, glossary terms

--- src/vbtetris/VBTetrisBlock.java
+++ src/vbtetris/VBTetrisBlock.java
@@ -4,8 +4,38 @@ import java.util.Arrays;
 
 import vbtetris.VBTetrisPieces.Tetrominoes;
 
+/** CSCI331 ER STATICBINDING
+ * 
+ * All of the methods and fields in this class will be statically bound.
+ * 
+ * private, final and static methods and variables uses static binding
+ * and are bonded by the compiler.
+ * 
+ * All the static method calls are resolved at compile time because
+ * static methods are accessed using the class name. Therefore there
+ * the compiler knows which version of the method is going to be called. 
+ * 
+ */
 public class VBTetrisBlock
 {	
+	
+	/** CSCI331 ER ENCAPSULATION
+	 * 
+	 * In this class, all the fields are private and can only be
+	 * accessed using the public getters and setters.
+	 * 
+	 * Although the getters and setters in this class don't do
+	 * anything special (like validation), using encapsulation
+	 * allows me to easily change implimentation in the future,
+	 * if necessary, without changing every file in the project.
+	 * 
+	 * One of the main reasons for not using public member variables
+	 * is that if something goes wrong with a public field the problem
+	 * can possibly be anywhere in the code, whereas if the field is
+	 * private there is only one place to look for the bug (right here
+	 * in this class).
+	 * 
+	 */
 	private boolean isEmpty;									// is the block occupied
 	private Tetrominoes shape;									// shape block belongs to
 	private int owner;											// owner of the block
@@ -27,16 +57,10 @@ public class VBTetrisBlock
     public void setOwner(int player) {owner = player;}
     public int getOwner() {return owner;}
     
-    // set the x position of the piece block
     public void setX(int x) {blockCoords[0] = x;}
-    
-    // set the y position of the piece block
-    public void setY(int y) {blockCoords[1] = y;}
-    
-    // get the x position of the piece block
     public int getX() {return blockCoords[0];}
     
-    // get the y position of the piece block
+    public void setY(int y) {blockCoords[1] = y;}
     public int getY() {return blockCoords[1];}
     
     public void setBlockCoords(int[] coords) {
